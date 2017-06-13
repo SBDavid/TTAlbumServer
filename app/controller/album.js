a242542239b4a1781f7efe98bd5207825760c6ec
davidtangjw <davidtangjw@pptv.com>
2017-06-13 18:07:50 +0800

    修改测试数据

--- app/controller/album.js
+++ app/controller/album.js
@@ -18,9 +18,9 @@ var res =
                 template: 'SimpleList',
                 id: 2,
                 data: [
-                    'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com/static1/img/david/1.jpg',
-                    'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com/static1/img/david/2.jpg',
-                    'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com/static1/img/david/3.jpg',
+                    'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com:81/static1/img/david/1.jpg',
+                    'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com:81/static1/img/david/2.jpg',
+                    'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com:81/static1/img/david/3.jpg',
                 ]
             }
         ]

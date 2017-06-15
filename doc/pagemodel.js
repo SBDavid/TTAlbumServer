efd500e1727dea28f31e2a6a9142972e60ec857c
davidtangjw <davidtangjw@pptv.com>
2017-06-15 15:30:07 +0800

    数据模型文档

---
+++ doc/pagemodel.js
@@ -0,0 +1,29 @@
+
+var urlPage_Table = {
+    url: "url匹配规则",
+    pageId: "页面Id",
+    onlineDate: "上线时间",
+    offlineDate: "下线时间" 
+},
+
+var page_Model = {
+    head: {
+        title: "title",
+        meta: [],
+    },
+    body: {
+        node:['nodeId','nodeId']
+    }
+}
+
+var pageNode_Table = [
+    {
+        nodeId: 'nodeId',
+        templateId: 'templateId',
+        onlineDate: "上线时间",
+        offlineDate: "下线时间",
+        data: {
+            /*模板数据*/
+        }
+    }
+]

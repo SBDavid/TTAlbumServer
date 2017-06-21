21dc3530edc98cd3d9fcf332614172cd62020ba9
SBDavid <billl89@126.com>
2017-06-21 21:32:11 +0800

    修改文档

--- doc/albummodel.js
+++ doc/albummodel.js
@@ -4,6 +4,8 @@ const User = {
   albums: [
     {
       name: 'albumName',
+      title: '照片标题',
+      surface: 'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com:81/static1/img/david/1.jpg',
       photos: [
         {
           src: 'http://ec2-52-15-52-128.us-east-2.compute.amazonaws.com:81/static1/img/david/1.jpg',
